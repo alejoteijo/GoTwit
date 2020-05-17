e3c9addd0a7829f1d1d6a0b553cd0bad091df65b
alejoteijo <alejoteijo@outlook.com>
2020-05-17 04:16:20 +0200

    SignUp function

---
+++ routers/signUp.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+	"encoding/json"
+	"github.com/alejoteijo/GoTwit/models"
+	"net/http"
+)
+
+//SignUp user sign up
+func SignUp(response http.ResponseWriter, request *http.Request) {
+	var t models.Usuario
+	error := json.NewDecoder(request.Body)
+}
