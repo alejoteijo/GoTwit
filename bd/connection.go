138898ca1103795c435232ef3bea9a8d3d5616b0
alejoteijo <alejoteijo@outlook.com>
2020-05-17 02:56:04 +0200

    Connectiono to MongoDB

---
+++ bd/connection.go
@@ -0,0 +1,37 @@
+package bd
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//MongoConnection is the received MongoDB client
+var MongoConnection = ConnectBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://alejoteijo:oJoIVSagZMFEnBpe@cluster0-4utd5.mongodb.net/test?retryWrites=true&w=majority")
+
+//ConnectBD Try to connect to a MongoDB
+func ConnectBD() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, error := mongo.Connect(ctx, clientOptions)
+	checkError(error)
+	error = client.Ping(ctx, nil)
+	checkError(error)
+	log.Println("Connection Succesful")
+	return client
+}
+
+func checkError(error error) {
+	if error != nil {
+		log.Fatal(error)
+	}
+}
+
+func checkConnection() bool {
+	error := MongoConnection.Ping(context.TODO(), nil)
+	return error == nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/alejoteijo/GoTwit/bd"
+	"github.com/alejoteijo/GoTwit/handlers"
+	"log"
+)
+
 func main() {
-	
 }
