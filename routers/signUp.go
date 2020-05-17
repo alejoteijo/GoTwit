48115b073275a0e5a5b42cc9c5a46d61071ed3d9
alejoteijo <alejoteijo@outlook.com>
2020-05-17 04:51:51 +0200

    SignUp function

--- routers/signUp.go
+++ routers/signUp.go
@@ -1,13 +1,48 @@
 package routers
 
 import (
-	//"encoding/json"
-	//"github.com/alejoteijo/GoTwit/models"
+	"encoding/json"
+	"github.com/alejoteijo/GoTwit/bd"
+	"github.com/alejoteijo/GoTwit/models"
 	"net/http"
 )
 
 //SignUp user sign up
 func SignUp(response http.ResponseWriter, request *http.Request) {
-	//var t models.Usuario
-	//error := json.NewDecoder(request.Body)
+	var t models.User
+	error := json.NewDecoder(request.Body).Decode(&t)
+	if error != nil {
+		http.Error(response, "Received data Error "+error.Error(), 400)
+		return
+	}
+
+	if len(t.Email) == 0 {
+		http.Error(response, "Required Email", 400)
+		return
+	}
+
+	if len(t.Password) < 6 {
+		http.Error(response, "Password must be at least 6 characters", 400)
+		return
+	}
+
+	_, finded, _ := bd.CheckUserExists(t.Email)
+	if finded == true {
+		http.Error(response, "User already exists in database", 400)
+		return
+	}
+
+	_, status, error := bd.AddRegister(t)
+	if error != nil {
+		http.Error(response, "Sign Up user error "+error.Error(), 400)
+		return
+	}
+
+	if status == false {
+		http.Error(response, "Can't add user", 400)
+		return
+	}
+
+	response.WriteHeader(http.StatusCreated)
+
 }
