004516d2d9257e1df6117d7efca2e2d32503eb22
alejoteijo <alejoteijo@outlook.com>
2020-05-17 03:50:35 +0200

    User model

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/alejoteijo/GoTwit/bd"
 	"github.com/alejoteijo/GoTwit/handlers"
+	"log"
 )
 
 func main() {
+	if !bd.CheckConnection() {
+		log.Fatal("Can't connect to database")
+	}
 	handlers.Handlers()
 }
